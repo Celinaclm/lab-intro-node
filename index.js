32a43ebd1184d393b5acce579396c9f79d9cc316
Celinaclm <celinaclm@gmail.com>
2021-04-12 12:00:35 +0200

    test passed

--- index.js
+++ index.js
@@ -1,17 +1,43 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = '';
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(!this.items[pos]) throw new Error('OutOfBounds');
+    return this.items[pos];
+  }
 
-  max() {}
+  max() {
+    if(this.length === '') throw new Error('EmptySortedList');
+    let pos = this.length -1;
+    return this.items[pos];
+  }
 
-  min() {}
+  min() {
+    if(this.length === '') throw new Error('EmptySortedList');
+    return this.items[0];
+  }
 
-  sum() {}
+  sum() {
+    if(this.length === '') return this.length;
+    let sum = this.items.reduce((acc, item) => {
+    return acc += item;
+    });
+    return sum;
+  }
 
-  avg() {}
+  avg() {
+    if(this.length === '') throw new Error('EmptySortedList');
+    return this.sum() / this.length;
+  }
 }
 
 module.exports = SortedList;
